2a2a0269e2450c9afecee57890511903c8cf7f6f
Simkort <sim465vman@live.dk>
2022-02-16 12:00:55 +0100

    Changes by Kenneth

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,9 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Main {
 
+    private static ConcurrentLinkedQueue<TimeSeriesReading> MAIN_TIMESERIES_RECORDING_BUFFER;
+
+
     public static void main(String[] args) {
         initializeMainBuffer();
         initializeDataReceivers();
@@ -18,4 +21,19 @@ public class Main {
         }
     }
 
+
+    private static void initializeMainBuffer(){
+        MAIN_TIMESERIES_RECORDING_BUFFER = new ConcurrentLinkedQueue<>();
+    }
+
+
+    private static void initializeDataReceivers() {
+        if (MAIN_TIMESERIES_RECORDING_BUFFER == null) {
+            throw new IllegalStateException("buffer must be initialized before initializing data receivers");
+        }
+
+        CSVDataReceiver csvDataReceiver = new CSVDataReceiver("src/main/resources/test.csv", MAIN_TIMESERIES_RECORDING_BUFFER, ",");
+
+        csvDataReceiver.run();
+    }
 }

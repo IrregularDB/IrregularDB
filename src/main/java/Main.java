01311773194eea7ef1623885d5c444eb44e9e446
KennethNorholm <70717116+KennethNorholm@users.noreply.github.com>
2022-02-17 11:22:24 +0100

    cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,6 @@ import scheduling.WorkingSetFactory;
 import sources.CSVDataReceiverSpawner;
 import sources.DataReceiverSpawner;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
-
-
 public class Main {
 
     public static void main(String[] args) {

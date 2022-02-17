1894240c6a25ed3225de496c26c13625fac150ad
EsbenNedergaard <esben.nedergaard@yahoo.dk>
2022-02-17 09:47:55 +0100

    updated time stamp compression model

--- src/main/java/compression/timestamp/TimeStampCompressionModel.java
+++ src/main/java/compression/timestamp/TimeStampCompressionModel.java
@@ -1,5 +1,4 @@
 package compression.timestamp;
-import records.DataPoint;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -14,37 +13,61 @@ enum TimeStampCompressionModelType {
 
 public abstract class TimeStampCompressionModel {
     protected double errorBound;
-    protected List<DataPoint> dataPoints;
+    protected List<Long> timeStamps;
 
     public TimeStampCompressionModel(double errorBound) {
+        this.resetEntireModel();
         this.errorBound = errorBound;
-        this.dataPoints = new ArrayList<>();
     }
 
-    public int getLength() {
-        return dataPoints.size();
+    protected abstract void resetModelParameters();
+
+    protected final void resetEntireModel() {
+        this.timeStamps = new ArrayList<>();
+        this.resetModelParameters();
     }
 
-    public double getCompressionRatio() {
-        throw new RuntimeException("Not tested but idea for implementation in abstract class");
-        // THE BYTE BUFFER HAS NOT SIZE SO WE NEED TO DO SOMETHING ELSE
-        //return (double)this.getLength() / (double)(this.getValueBlob().position());
+    public final int getLength() {
+        return timeStamps.size();
     }
 
-    public abstract boolean appendDatapoint(DataPoint dataPoint);
-    public abstract boolean appendBuffer(List<DataPoint> dataPointBuffer);
+    public abstract boolean appendTimeStamp(long timeStamp);
+
+    /**
+     * Is used to reset the model and append a series of data points to it
+     * often used right after emitting a segment to fill it with the buffer again
+     * @return returns true if the entire time stamp list could be appended
+     */
+    public final boolean resetAndAppendAll(List<Long> timeStamps) {
+        this.resetEntireModel();
+
+        boolean appendSucceeded = false;
+        for (Long timeStamp : timeStamps) {
+            appendSucceeded = this.appendTimeStamp(timeStamp);
+            if (!appendSucceeded) {
+                break;
+            }
+        }
+        return appendSucceeded;
+    }
 
     /**
      * Byte list representation of the model that should be saved to the database
      * @return ByteBuffer class from java
      */
-    public abstract ByteBuffer getValueBlob();
+    public abstract ByteBuffer getTimeStampBlob();
 
     /**
      * Used to identify which type of model is used when writing to the DB
      * @return enum for the value compression model types
      */
-    public abstract TimeStampCompressionModelType getValueCompressionModelType();
+    public abstract TimeStampCompressionModelType getTimeStampCompressionModelType();
+
+    public final double getCompressionRatio() {
+        throw new RuntimeException("Not tested but idea for implementation in abstract class");
+        // THE BYTE BUFFER HAS NOT SIZE SO WE NEED TO DO SOMETHING ELSE
+        //return (double)this.getLength() / (double)(this.getValueBlob().position());
+    }
 
     /**
      * This method is used when joining time stamp and value compression models

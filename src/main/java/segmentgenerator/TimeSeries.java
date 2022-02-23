8c073c3819cbcf203d9ebd19904c6e2fde09c40a
Simkort <sim465vman@live.dk>
2022-02-23 14:10:54 +0100

    Changes by Kenneth - postgres setup

--- src/main/java/segmentgenerator/TimeSeries.java
+++ src/main/java/segmentgenerator/TimeSeries.java
@@ -3,15 +3,21 @@ package segmentgenerator;
 import compression.CompressionModelFactory;
 import records.DataPoint;
 import records.Segment;
+import storage.DatabaseConnection;
+import storage.PostgresConnection;
 
 public class TimeSeries {
 
-    private final String timeSeriesKey;
+    private final String timeSeriesTag;
+    private final int timeSeriesId;
+    private final DatabaseConnection databaseConnection;
     private final SegmentGenerator segmentGenerator;
 
     public TimeSeries(String timeSeriesKey) {
-        this.timeSeriesKey = timeSeriesKey;
-        this.segmentGenerator = new SegmentGenerator(new CompressionModelManager(CompressionModelFactory.getValueCompressionModels(), CompressionModelFactory.getTimestampCompressionModels()), timeSeriesKey);
+        this.timeSeriesTag = timeSeriesKey;
+        this.databaseConnection = new PostgresConnection();
+        this.timeSeriesId = databaseConnection.getTimeSeriesId(this.timeSeriesTag);
+        this.segmentGenerator = new SegmentGenerator(new CompressionModelManager(CompressionModelFactory.getValueCompressionModels(), CompressionModelFactory.getTimestampCompressionModels()), timeSeriesId);
     }
 
     public void processDataPoint(DataPoint dataPoint) {
@@ -22,7 +28,7 @@ public class TimeSeries {
     }
 
     public String getTimeSeriesKey() {
-        return timeSeriesKey;
+        return timeSeriesTag;
     }
 
     void sendToDb(Segment segment) {

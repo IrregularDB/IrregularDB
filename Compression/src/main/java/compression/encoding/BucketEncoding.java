20b20edcfc501cad0f867be488dd231c341f2555
EsbenNedergaard <esben.nedergaard@yahoo.dk>
2022-03-31 12:00:55 +0200

    Made new branch with BR about SI-diff

--- Compression/src/main/java/compression/encoding/BucketEncoding.java
+++ Compression/src/main/java/compression/encoding/BucketEncoding.java
@@ -39,6 +39,18 @@ public class BucketEncoding {
         return bitBuffer;
     }
 
+    public List<Integer> getAbsoluteMaxValuesOfResizeableBuckets() {
+        List<Integer> maxValues = new ArrayList<>();
+
+        // We have bucket_0, which is same value
+        maxValues.add(0);
+
+        maxValues.add((int)((long) Math.pow(2, BUCKET_1_BIT_SIZE)) - 1);
+        maxValues.add((int)((long) Math.pow(2, BUCKET_2_BIT_SIZE)) - 1);
+        // we don't add the last bucket as it is the largest bucket
+        return maxValues;
+    }
+
     private void encodeReading(Integer reading, Integer prevReading) {
         if (reading.equals(prevReading)) {
             writeControlBitsToBuffer(SAME_VALUE_ENCODING, bitBuffer);
--- Compression/src/main/java/compression/timestamp/SIDiffTimeStampCompressionModel.java
+++ Compression/src/main/java/compression/timestamp/SIDiffTimeStampCompressionModel.java
@@ -9,40 +9,76 @@ import java.util.List;
 
 public class SIDiffTimeStampCompressionModel extends TimeStampCompressionModel {
     private final SignedBucketEncoder signedBucketEncoder;
-    private Long firstTimeStamp;
-    private List<Long> timeStamps;
+    private List<Long> timestamps;
 
-
-    public SIDiffTimeStampCompressionModel() {
-        super(null, null);
+    public SIDiffTimeStampCompressionModel(float errorBound) {
+        super(errorBound, null);
+        // We make this a field so that we don't have to allocate a new signed bucket encoder each time get byte buffer is called
         signedBucketEncoder = new SignedBucketEncoder();
         this.resetModel();
     }
 
     @Override
     protected void resetModel() {
-        this.timeStamps = new ArrayList<>();
-        this.firstTimeStamp = null;
+        this.timestamps = new ArrayList<>();
     }
 
     @Override
     public int getLength() {
-        return timeStamps.size();
+        return timestamps.size();
     }
 
     @Override
     protected boolean appendDataPoint(DataPoint dataPoint) {
-        return false;
+        timestamps.add(dataPoint.timestamp());
+        return true;
     }
 
     @Override
     protected ByteBuffer createByteBuffer() {
-        return null;
+        List<Integer> readings = new ArrayList<>();
+
+        long firstTimestamp = timestamps.get(0);
+        int si = calculateSI(firstTimestamp);
+        readings.add(si);
+
+        int allowedDerivation = (int)(si * getErrorBound());
+        List<Integer> maxValuesOfBuckets = signedBucketEncoder.getAbsoluteMaxValuesOfResizeableBuckets();
+
+        // We skip the first timestamp as it is stored on the segment
+        for (int i = 1; i < timestamps.size(); i++) {
+            long approximation = firstTimestamp + (long) si * i;
+            int difference = calculateDifference(timestamps.get(i), approximation, maxValuesOfBuckets, allowedDerivation);
+            readings.add(difference);
+        }
+        return signedBucketEncoder.encode(readings).getFinishedByteBuffer();
+    }
+
+    private int calculateSI(long firstTimestamp) {
+        long lastTimestamp = timestamps.get(timestamps.size() - 1);
+
+        long duration = lastTimestamp - firstTimestamp;
+        return Math.round((float) duration / (timestamps.size() - 1));
+    }
+
+    private int calculateDifference(long currentTimestamp, long approximation, List<Integer> maxValuesOfBuckets, int allowedDerivation) {
+        int difference = Math.toIntExact(currentTimestamp - approximation);
+        int absoluteValueOfDifference = Math.abs(difference);
+
+        for (var maxValue : maxValuesOfBuckets) {
+            // We look for values that are between max value and max value + allowed derivation
+            if (maxValue <= absoluteValueOfDifference && absoluteValueOfDifference <= (maxValue + allowedDerivation)) {
+                boolean isNegativeNumber = difference < 0;
+                difference = isNegativeNumber ? -1 * maxValue : maxValue;
+                break;
+            }
+        }
+        return difference;
     }
 
     @Override
     protected void reduceToSize(int n) {
-
+        this.timestamps.subList(n, timestamps.size()).clear();
     }
 
     @Override

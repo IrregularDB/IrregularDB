d86d59b1682bf509a7fdfebba8f08baaa700e7a3
Simkortet <sim465vman@live.dk>
2022-03-31 09:54:17 +0200

    Added some function annotations because otherwise stuff doesn't work :o

---
+++ PostgresUDF/src/main/java/PLTest.java
@@ -0,0 +1,28 @@
+import org.postgresql.pljava.annotation.Function;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class PLTest {
+
+    @Function
+    public static Integer test(Integer inputString){
+        return inputString + 1;
+    }
+
+    @Function
+    public static String hello(String toWhom) {
+        return "Hello, " + toWhom + "!";
+    }
+    //FOR ME: <>
+    @Function
+    public static Iterator<String> hellos(String toWhom) {
+        ArrayList<String> stringArray = new ArrayList<>();
+
+        for (int i = 0; i != 10; i++){
+            stringArray.add(toWhom);
+        }
+
+        return stringArray.iterator();
+    }
+}
